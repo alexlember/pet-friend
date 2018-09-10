5bfbfb78f60a742ea7e12ac9a8156fc8a40673cd
Алексей Лемберский <alexlember@gmail.com>
2018-09-11 00:00:33 +0300

    1 - init infrastructure

---
+++ src/main/java/ru/lember/petfriend/PetFriendApplication.java
@@ -0,0 +1,12 @@
+package ru.lember.petfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFriendApplication.class, args);
+	}
+}
